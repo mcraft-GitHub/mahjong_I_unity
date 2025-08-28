ad046aac17d4a14ee5785d3e3a66a38ea6ba9b70
mc-imaeda <imaeda@m-craft.com>
2025-08-28 17:37:25 +0900

    間違えて消したタイトルを追加

---
+++ Mahjong/Assets/Mahjong/Scripts/Title/TitleManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TitleManager : MonoBehaviour
+{
+    private const float FADE_TIME = 1.0f;
+
+    [SerializeField] private Image _fadeImage;
+
+    void Start()
+    {
+        // フェードイン
+        _fadeImage.color = new Color(0.0f, 0.0f, 0.0f, 1.0f);
+        _fadeImage.DOColor(new Color(0.0f, 0.0f, 0.0f, 0.0f), FADE_TIME);
+    }
+
+    public void ToGameScene()
+    {
+        // ゲームシーンへ遷移
+        StartCoroutine(LoadScene("GameScene"));   
+    }
+
+    IEnumerator LoadScene(string sceneName)
+    {
+        // フェードアウト
+        yield return _fadeImage.DOColor(new Color(0.0f, 0.0f, 0.0f, 1.0f), FADE_TIME).WaitForCompletion();
+        SceneManager.LoadScene(sceneName);
+    }
+}
