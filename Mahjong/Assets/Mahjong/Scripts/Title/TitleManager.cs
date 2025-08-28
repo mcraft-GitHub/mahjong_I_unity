f559040da7b8a560c3f17fbf0443703cb7993845
mc-imaeda <imaeda@m-craft.com>
2025-08-28 15:59:51 +0900

    タイトルスクリプト(シーン遷移)

---
+++ Mahjong/Assets/Mahjong/Scripts/Title/TitleManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TitleManager : MonoBehaviour
+{
+    private const float FADE_TIME = 1.0f;
+
+    [SerializeField] private Image _fadeImage;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ToGameScene()
+    {
+        // ゲームシーンへ遷移
+        StartCoroutine(LoadScene(1));   
+    }
+
+    IEnumerator LoadScene(int buildIndex)
+    {
+        yield return _fadeImage.DOColor(new Color(0.0f, 0.0f, 0.0f, 1.0f), FADE_TIME).WaitForCompletion();
+        SceneManager.LoadScene(buildIndex);
+    }
+}
