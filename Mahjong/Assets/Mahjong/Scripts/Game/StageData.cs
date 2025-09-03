ce69b72331d32e7f84379c27f3a81686e4751916
mc-imaeda <imaeda@m-craft.com>
2025-09-03 16:34:14 +0900

    ステージデータ追加

---
+++ Mahjong/Assets/Mahjong/Scripts/Game/StageData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StageData", menuName = "Game/StageData")]
+public class StageData : ScriptableObject
+{
+    // 名前
+    public string _name;
+
+    // 説明文
+    public string _description;
+
+    // 難易度
+    public int _difficulty;
+
+    // 出現敵
+    public List<EnemyData> _appearEnemy;
+
+    // 使用牌
+    public List<MahjongLogic.TILE_KIND> _useTilesKind;
+
+    // 報酬(未定)
+}
